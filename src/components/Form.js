4b797c87082dbfb8f4473da38efb8a800b27869b
dMuguti <omiahderek56@gmail.com>
2023-04-23 11:13:41 +0300

    Done

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,14 +13,20 @@ function Form() {
     setLastName(event.target.value);
   }
 
+  const [errors, setErrors] = useState([]);
   function handleSubmit(event) {
     event.preventDefault();
+     if (firstName.length > 0) {
     const formData = { firstName: firstName, lastName: lastName };
     const dataArray = [...submittedData, formData];
     setSubmittedData(dataArray);
     setFirstName("");
     setLastName("");
+    setErrors([]);
+  } else {
+    setErrors(["First name is required!"]);
   }
+}
 
   const listOfSubmissions = submittedData.map((data, index) => {
     return (
@@ -37,6 +43,14 @@ function Form() {
         <input type="text" onChange={handleLastNameChange} value={lastName} />
         <button type="submit">Submit</button>
       </form>
+      {/* conditionally render error messages */}
+    {errors.length > 0
+      ? errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))
+      : null}
       <h3>Submissions</h3>
       {listOfSubmissions}
     </div>
